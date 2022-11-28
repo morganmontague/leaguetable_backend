8fc732aa78f6df232baeedcc9155e5035ba3acea
C. Morgan Montague <morgan.montague@student.awesomeinc.org>
2022-11-28 20:24:00 +0000

    added the teams model

--- leaguetable/leaguetable/leaguetable/settings.py
+++ leaguetable/leaguetable/leaguetable/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tableapi',
     'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
--- leaguetable/leaguetable/tableapi/models.py
+++ leaguetable/leaguetable/tableapi/models.py
@@ -6,3 +6,19 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=200, default="New Team")
+    team_grounp = models.PositiveIntegerField()
+    games_played = models.PositiveIntegerField()
+    user_id = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name="manager_name")
+    # location_id
+    created_at = models.DateTimeField(auto_now_add=True, null=True)  
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    wins = models.PositiveIntegerField()
+    losses = models.PositiveIntegerField()
+    ties = models.PositiveIntegerField()
+    goals_scored = models.PositiveIntegerField()
+    rank = models.PositiveIntegerField()
+    points =models.PositiveIntegerField()
