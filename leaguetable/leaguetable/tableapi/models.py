ff559a85f884ed0cbf49c8374e9ccc977c5d337d
C. Morgan Montague <morgan.montague@student.awesomeinc.org>
2022-11-29 20:38:54 +0000

    made new team players model with foriegn keys, also new branch

--- leaguetable/leaguetable/tableapi/models.py
+++ leaguetable/leaguetable/tableapi/models.py
@@ -12,7 +12,7 @@ class CustomUser(AbstractUser):
 class Team(models.Model):
     team_name = models.CharField(max_length=200, default="New Team")
     team_grounp = models.PositiveIntegerField()
-    user_id = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name="manager_name")
+    user_id = models.ForeignKey('CustomUser', on_delete=models.PROTECT, related_name="manager_name")
     # location_id
     created_at = models.DateTimeField(auto_now_add=True, null=True)  
     updated_at = models.DateTimeField(auto_now=True, null=True)
@@ -31,13 +31,13 @@ class Player(models.Model):
     player_name = models.CharField(max_length=200, default="new guy")
     # team_id 
     jersey_no = models.PositiveIntegerField(validators= [MaxValueValidator(99)])
-    nationality_id = models.ForeignKey('nationality', on_delete=models.CASCADE, related_name="player_nation", null=True)
-    position_id = models.ForeignKey('position', on_delete=models.CASCADE, related_name="player_position", null=True)
+    nationality_id = models.ForeignKey('nationality', on_delete=models.PROTECT, related_name="player_nation", null=True)
+    position_id = models.ForeignKey('position', on_delete=models.PROTECT, related_name="player_position", null=True)
     injured = models.BooleanField()
     age = models.PositiveIntegerField(validators= [MinValueValidator(16), MaxValueValidator(45)])
 
     def __str__(self):
-        return self.player_name + ': ' + str(self.jersey_no)
+        return self.player_name + ': Jersey Number is ' + str(self.jersey_no)
 
 class Position(models.Model):
     position = models.CharField(max_length=100, default='Soccer Player')
@@ -50,3 +50,7 @@ class Nationality(models.Model):
 
     def __str__(self):
         return self.nationality
+
+class team_players(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.PROTECT)
+    team = models.ForeignKey(Team, on_delete=models.PROTECT)
